647848b1a551e3980c8940e304d2b702644b5a88
anbukkannan6 <anbukkannan6@gmail.com>
2023-10-14 18:24:19 +0530

    Almost Completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tic-tac-game
+
+# youtube link
+https://www.youtube.com/watch?v=UKfy2z8WEhU
--- script.js
+++ script.js
@@ -18,24 +18,79 @@ const winningMessageTextElement = document.getElementById("restartButton");
 
 let circleTurn;
 
-startGame()
+startGame();
+restartButton.addEventListener("click",startGame);
 
-function startGame(){
+function startGame() {
     circleTurn = false;
-    cellElements.forEach((cell)=>{
+    cellElements.forEach((cell) => {
         cell.classList.remove(X_CLASS);
         cell.classList.remove(CIRCLE_CLASS);
-        cell.removeEventListener("click",handleClick);
-        cell.addEventListener("click",handleClick,{once:true});
-    })
+        cell.removeEventListener("click", handleClick);
+        cell.addEventListener("click", handleClick, { once: true });
+    });
+    setBoardHover();
+    winningMessage.classList.remove("show")
 }
 
-function handleClick(e){
+function handleClick(e) {
     const cell = e.target;
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-    placeMark(cell,currentClass)
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false)
+
+    }
+    else if(isDraw()){
+        endGame(true)
+    }
+    else{
+        swapTurns();
+        setBoardHover();
+    }
 }
 
-function placeMark(cell,currentClass){
+function swapTurns(){
+    circleTurn = !circleTurn;
+}
+
+function setBoardHover(){
+    board.classList.remove(CIRCLE_CLASS);
+    board.classList.remove(X_CLASS);
+    if(circleTurn){
+        board.classList.add(CIRCLE_CLASS);
+    }
+    else{
+        board.classList.add(X_CLASS);
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerHTML = "Drawn!"
+    }
+    else {
+        winningMessageTextElement = `${circleTurn ? "0's" : "x's"} wins`
+    }
+    winningMessage.classList.add(show);
+}
+
+function placeMark(cell, currentClass) {
     cell.classList.add(currentClass);
+}
+
+function isDraw(){
+    return [...cellElements].every((cell)=>{
+        return (
+            cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+        );
+    });
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATION.some((combination) => {
+        return combination.every((index) => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
 }
